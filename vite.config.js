e5a86d2afe3f0edd5c6d2bbfd607f300198e029e
Reza <118562448+Ibrahim71Reza@users.noreply.github.com>
2025-06-11 18:32:50 +0600

    Confirm correct base path in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Add this line
+  // This must match your repository name
+  base: '/Portfolio/',
 })
